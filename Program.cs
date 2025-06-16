b116e61e6f81dafabf5f4d89c508dd2916b88b0c
HuyPham02 <huyplnse183324@fpt.edu.vn>
2025-06-16 22:31:23 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using GenderHealthSystem.Data;
+using GenderHealthSystem.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Đăng ký DbContext với chuỗi kết nối từ appsettings.json
+builder.Services.AddDbContext<GenderhealthContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Đăng ký Repository và Service
+// Register Repositories as Scope
+builder.Services.AddScoped<IServiceRepository, ServiceRepositoryImpl>();
+builder.Services.AddScoped<IWebUserRepository, WebUserRepositoryImpl>();
+
+
+// Cấu hình Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "GenderHealthSystem API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GenderHealthSystem API v1"));
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
