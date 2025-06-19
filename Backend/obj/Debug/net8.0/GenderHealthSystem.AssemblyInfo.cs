942d1ecc85df64a84dae38abe1771bbbb2643cc6
nguyenthilocnhi <nhintlse182735@fpt.edu.vn>
2025-06-20 01:26:57 +0700

    Update code

--- Backend/obj/Debug/net8.0/GenderHealthSystem.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/GenderHealthSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GenderHealthSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f86082a962046173abf3b9d5f875526463df74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38546bee9901cfd269b362bdeda0b6d9df3125d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GenderHealthSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GenderHealthSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
